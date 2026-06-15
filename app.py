3a70e7b76e8062486557552637c2ac341e0d3095
Aastha Shetty <aasshetty12@gmail.com>
2026-06-15 11:32:59 +0530

    Fix Render port binding

--- app.py
+++ app.py
@@ -51,7 +51,8 @@ def delete_student(id):
     return redirect('/students')
 
 
+import os
+
 if __name__ == '__main__':
-    app.run()
-    
-app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
