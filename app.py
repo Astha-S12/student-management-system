3cb1d8b0036d0dd19073c9e0386efe7572f8a159
Aastha Shetty <aasshetty12@gmail.com>
2026-06-15 12:06:30 +0530

    test home route

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # ---------------- HOME ----------------
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return "Flask is working on Render"
 
 # ---------------- ABOUT ----------------
 @app.route('/about')
