1ca514b182c4423646f09e96646ccf784ae601f6
Aastha Shetty <aasshetty12@gmail.com>
2026-06-16 11:16:15 +0530

    fix railway mysql connection

--- db.py
+++ db.py
@@ -2,11 +2,13 @@ import os
 import pymysql
 
 def get_db_connection():
+    url = os.getenv("mysql://root:DnLawNxZzqsGzPXWSWHLksNGkUtIkqFG@mysql.railway.internal:3306/railway")
+
     return pymysql.connect(
         host="thomas.proxy.rlwy.net",
         port=58444,
         user="root",
-        password=os.getenv("DnLawNxZzqsGzPXWSWHLksNGkUtIkqFG"),
+        password="DnLawNxZzqsGzPXWSWHLksNGkUtIkqFG",
         database="railway",
         cursorclass=pymysql.cursors.DictCursor
     )
