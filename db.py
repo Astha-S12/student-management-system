5a6be40a726f2188a230b269ffe5d1857204808c
Aastha Shetty <aasshetty12@gmail.com>
2026-06-16 10:36:49 +0530

    use render env vars

--- db.py
+++ db.py
@@ -1,19 +1,12 @@
+import os
 import pymysql
 
 def get_db_connection():
-    try:
-        conn = pymysql.connect(
-            host="mysql.railway.internal",
-            user="root",
-            password="DnLawNxZzqsGzPXWSWHLksNGkUtIkqFG",
-            database="railway",
-            port=3306,
-            cursorclass=pymysql.cursors.DictCursor
-        )
-        print("✅ DB CONNECTED")
-        return conn
-
-    except Exception as e:
-        print("❌ DB ERROR:", e)
-        return None
-    
+    return pymysql.connect(
+        host=os.getenv("thomas.proxy.rlwy.net"),
+        user=os.getenv("root"),
+        password=os.getenv("DnLawNxZzqsGzPXWSWHLksNGkUtIkqFG"),
+        database=os.getenv("railway"),
+        port=int(os.getenv(58444)),
+        cursorclass=pymysql.cursors.DictCursor
+    )
